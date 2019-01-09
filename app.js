8bd28133b30e03047e82b24e3f4330ecaf80e6f3
lohith <viperlohith121@gmail.com>
2019-01-09 16:48:44 +0530

    External auth file

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+//routes
+const authRoutes = require('./routes/auth-routes')
+
 const app = express()
 
+//Using middleware
 app.use(bodyParser)
+
+//Routes middleware
+app.use('auth', authRoutes)
 
 app.get('/', (req, res, next) => {
     res.send('Welcome to express app')
